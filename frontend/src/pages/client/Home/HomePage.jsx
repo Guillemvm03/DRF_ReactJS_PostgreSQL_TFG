3456261466664335aaa640f99214b35ba7baa836
Guillem <guillemvicent09@gmail.com>
2024-03-24 17:12:03 +0100

    i18n implemented react

--- frontend/src/pages/client/Home/HomePage.jsx
+++ frontend/src/pages/client/Home/HomePage.jsx
@@ -10,6 +10,7 @@ import { useNavigate } from "react-router-dom";
 
 import secrets from "../../../secrets";
 import { useStations } from "../../../hooks/useStations";
+import { useTranslation } from "react-i18next";
 
 const containerStyle = {
   width: "auto",
@@ -17,6 +18,8 @@ const containerStyle = {
 };
 
 const HomePage = () => {
+  const { t } = useTranslation(); // Obtiene la función t() para traducción
+
   const [mapL, setMapL] = useState(0);
   const [mapLN, setMapLN] = useState(0);
 
@@ -36,7 +39,7 @@ const HomePage = () => {
     },
     (error) => {
       console.log(error.message);
-      alert(error.message);
+      // alert(error.message);
     },
     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
   );
@@ -98,18 +101,17 @@ const HomePage = () => {
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            We invest in the world’s potential
+          {t('VIEWS.HOME.title')}
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-            Here at Flowbite we focus on markets where technology, innovation,
-            and capital can unlock long-term value and drive economic growth.
+          {t('VIEWS.HOME.content')}
           </p>
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <a
               href="#"
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
             >
-              Get started
+              {t('VIEWS.HOME.get-started')}
               <svg
                 className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
                 aria-hidden="true"
@@ -130,7 +132,7 @@ const HomePage = () => {
               href="#"
               className="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
-              Learn more
+            {t('VIEWS.HOME.learn-more')}
             </a>
           </div>
         </div>
--- frontend/src/pages/contact/ContactUs.jsx
+++ frontend/src/pages/contact/ContactUs.jsx
@@ -4,11 +4,13 @@ import { Button } from 'flowbite-react';
 import { useToastr } from '../../hooks/useToastr';
 import { useAuth } from '../../hooks/useAuth';
 import secrets from '../../secrets';
+import { useTranslation } from 'react-i18next'; // Importa useTranslation
 
 export default function ContactUs() {
     const form = useRef();
     const { useCreateToastr } = useToastr();
     const { user } = useAuth();
+    const { t } = useTranslation(); // Obtiene la función t() para traducción
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -19,38 +21,37 @@ export default function ContactUs() {
             })
             .then(
                 () => {
-                    useCreateToastr({ status: true , message: 'Email sent successfully'})
+                    useCreateToastr({ status: true , message: t('TOASTR.emailSent') }); 
                     form.current.reset();
                 },
                 (error) => {
                     console.log('FAILED...', error.text);
-                    useCreateToastr({ status: true, error: 'error', message: 'Email failed to send'})
+                    useCreateToastr({ status: true, error: 'error', message: t('TOASTR.emailFailedToSend') }); 
                 },
             );
     };
 
     return (
-
         <section className="bg-white dark:bg-gray-900">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">{t('VIEWS.CONTACT_US.title')}</h2> {/* Traduce el título utilizando la función t() */}
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{t('VIEWS.CONTACT_US.description')}</p> {/* Traduce la descripción utilizando la función t() */}
                 <form ref={form} onSubmit={sendEmail} className="space-y-8">
                     <div>
-                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
+                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{t('VIEWS.CONTACT_US.yourName')}</label> {/* Traduce el texto del label utilizando la función t() */}
                         <input type="text" id="name" name='user_name' defaultValue={user.username} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" disabled required />
                     </div>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{t('VIEWS.CONTACT_US.yourEmail')}</label> {/* Traduce el texto del label utilizando la función t() */}
                         <input type="email" id="email" name='user_email' defaultValue={user.email} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" disabled required />
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                        <textarea id="message" name='message' rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{t('VIEWS.CONTACT_US.yourMessage')}</label> {/* Traduce el texto del label utilizando la función t() */}
+                        <textarea id="message" name='message' rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={t('VIEWS.CONTACT_US.leaveComment')}></textarea> {/* Traduce el placeholder utilizando la función t() */}
                     </div>
-                    <Button color="blue" type='submit'>Send Message</Button>
+                    <Button color="blue" type='submit'>{t('VIEWS.CONTACT_US.sendMessage')}</Button> {/* Traduce el texto del botón utilizando la función t() */}
                 </form>
             </div>
         </section>
     );
-};
+}
